2442149ad5ec39a6f24227c4d5f62f6388afcd0e
sherzod_a_thinkland <sherzod_a@thinkland.uz>
2023-07-20 13:55:05 +0500

    production migrate

--- src/api/host.js
+++ src/api/host.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://localhost:8000'
+export const BASE_URL = 'squeamish-shape-production.up.railway.app:8000'
 
 export const TOKEN_ACCESS = 'course_project_access_token'
